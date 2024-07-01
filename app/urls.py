7674d703f1bfcc8bb5a86884beeb6afc066430a3
MeDudaSilva <mariaeduarda.11.me4@gmail.com>
2024-07-01 11:57:39 -0300

    Criação da model Cor e da API

--- app/urls.py
+++ app/urls.py
@@ -9,13 +9,14 @@ from drf_spectacular.views import (
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from core.views import AcessorioViewSet, CategoriaViewSet, UserViewSet
+from core.views import AcessorioViewSet, CategoriaViewSet, CorViewSet, UserViewSet
 
 router = DefaultRouter()
 
 router.register(r"acessórios", AcessorioViewSet)
 router.register(r"usuarios", UserViewSet, basename="usuarios")
 router.register(r"categorias", CategoriaViewSet)
+router.register(r"cor", CorViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
